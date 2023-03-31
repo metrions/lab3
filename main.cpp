c0895425b1b0cde95f5d56c0f40c80ba6ce3b6d6
Panarin Ilya <cccthafcdh@gmail.com>
2023-03-31 22:02:55 +0700

    last-vers

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 #include "tree.h"
-using namespace std;
+#include <fstream>
+#include "st2.h"
 
 
-int main() {
-    Node* root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
-
-    int count = countLeaves(root);
-    cout << endl << "Number of leaves: " << count << endl;
-    return 0;
+int main(){
+    std::ifstream input;
+    input.open("file.txt");
+    stack *st = new stack;
+    tree *t = BuildTree();
+    std::cout << vuvod(t, &st);
+    std::cout << "\n";
+    vuvod(st);
 }
--- tree.h
+++ tree.h
@@ -1,22 +1,66 @@
+#pragma once
+#include "st2.h"
+#include <fstream>
 #include <iostream>
-using namespace std;
 
+struct tree{
+    char el;
+    tree *right, *left;
 
-struct Node {
-    int val;
-    Node* left;
-    Node* right;
-    Node(int v) : val(v), left(nullptr), right(nullptr) {}
 };
 
-int countLeaves(Node* root) {
-    if (root == nullptr) {
-        return 0;
+tree *BuildTree()
+{
+    char c = 0;
+    scanf("%c", &c);
+    switch(c)
+    {
+        case '(': {
+            tree *t = new tree;
+            scanf("%c", &c);
+            t->el = c;
+            t->left = BuildTree();
+            t->right = BuildTree();
+            //std::cout << t->el;
+            scanf("%c", &c);
+            return t;
     }
-    if (root->left == nullptr && root->right == nullptr) {
-        cout << root->val << " ";
+        case ',': return BuildTree();
+        case '0': return NULL;
+        default: return BuildTree();
+    }
+    return NULL;
+}
+
+int findtree(tree *t, char x){
+    int f = 0;
+    if (t->el == x){
         return 1;
     }
-    int count = countLeaves(root->left) + countLeaves(root->right);
-    return count;
+    if (f!= 1 && t->right != NULL && t->left != NULL){
+        if (t->left != NULL){
+            t = t->left;
+            f = findtree(t, x);}
+        if (t->right != NULL){
+            t = t->right;
+            f = findtree(t, x);
+        }
+    }
+    return f;
+}
+
+int vuvod(tree *t, stack **s){
+    int f = 0;
+    if (t->right == NULL && t->left == NULL){
+        f = f + 1;
+        *s = push(t->el, *s);
+        return f;
+    }
+    if (t->left != NULL){
+        f = f + vuvod(t->left, s);
+    }
+    if (t->right != NULL){
+        f = f + vuvod(t->right, s);
+    }
+    return f;
 }
